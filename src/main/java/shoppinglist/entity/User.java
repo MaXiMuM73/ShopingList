a3568cd44e734587d093eae351f0dfecac29f1cd
Maksim Sunoplya <maksim.sunoplya@simbirsoft.com>
2021-10-26 15:56:58 +0400

    Changed names of sequences

--- src/main/java/shoppinglist/entity/User.java
+++ src/main/java/shoppinglist/entity/User.java
@@ -20,7 +20,7 @@ import java.util.Set;
 public class User {
 
     @Id
-    @SequenceGenerator(name = "userIdSeq", sequenceName = "user_sequence", allocationSize = 1)
+    @SequenceGenerator(name = "userIdSeq", sequenceName = "user_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userIdSeq")
     @Column(name = "id", nullable = false)
     private Long id;
