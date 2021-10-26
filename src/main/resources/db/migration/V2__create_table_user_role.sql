89b1a0d80f0003bf2deb51232a015fe2ffb853b8
Maksim Sunoplya <maksim.sunoplya@simbirsoft.com>
2021-10-26 15:55:16 +0400

    Refactored migrations

--- src/main/resources/db/migration/V2__create_table_user_role.sql
+++ src/main/resources/db/migration/V2__create_table_user_role.sql
@@ -1,22 +1,20 @@
-CREATE TABLE IF NOT EXISTS shopping.role
+CREATE TABLE shopping.role
 (
-    id bigint NOT NULL,
-    code character varying(255) NOT NULL,
-    CONSTRAINT role_pkey PRIMARY KEY (id)
+    id bigserial primary key NOT NULL,
+    code character varying(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS shopping.user_role
+CREATE TABLE shopping.user_role
 (
-    id serial NOT NULL,
+    id serial primary key NOT NULL,
     user_id bigint NOT NULL,
     role_id bigint NOT NULL,
-    CONSTRAINT user_role_pkey PRIMARY KEY (id),
     CONSTRAINT user_id FOREIGN KEY (user_id)
-    REFERENCES shopping.user (id) MATCH SIMPLE
+    REFERENCES shopping.user (id)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
     CONSTRAINT role_id FOREIGN KEY (role_id)
-    REFERENCES shopping.role (id) MATCH SIMPLE
+    REFERENCES shopping.role (id)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
